56502cda69325b330028e49fe83c0345634dc5d6
tsuyosasaki <tsuyo.sasaki@gmail.com>
2018-05-06 20:20:36 +0900

    ToDo削除機能、ToDoなしの表示を追加

--- dist/js/main.js
+++ dist/js/main.js
@@ -5,17 +5,31 @@
     el: '#app',
     data: {
       newItem: '',
-      todos: [
-        'task 1',
-        'task 2',
-        'task 3'
-      ]
+      // todos: [{
+      //   title: 'task 1',
+      //   isDone: false
+      // },{
+      //   title: 'task 2',
+      //   isDone: false
+      // },{
+      //   title: 'task 3',
+      //   isDone: true
+      // }]
+      todos: []
     },
     methods: {
       addItem: function(e) {
-        // e.preventDefault();
-        this.todos.push(this.newItem);
+        var item = {
+          title: this.newItem,
+          isDone: false
+        };
+        this.todos.push(item);
         this.newItem = '';
+      },
+      deleteItem: function(index){
+        if(confirm('are you sure?')){
+          this.todos.splice(index, 1);
+        }
       }
     }
   });
